42041af4436a01819247f5ceaae7df9c8b895634
Ilian Genov <ilian.genov@unwe.bg>
2022-07-08 13:51:05 +0300

    final final

--- controllers/RoomController.php
+++ controllers/RoomController.php
@@ -102,7 +102,7 @@ class RoomController extends Controller
         $gallery = new Gallery();
         $gallery->room_id = $model->id;
         $gallery->save();
-        return $this->redirect(['view', 'id' => $model->id]);
+        return $this->redirect(['index']);
       }
     } else {
       $model->loadDefaultValues();
@@ -125,7 +125,7 @@ class RoomController extends Controller
     $model = $this->findModel($id);
 
     if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-      return $this->redirect(['view', 'id' => $model->id]);
+      return $this->redirect(['index']);
     }
 
     return $this->render('update', [
